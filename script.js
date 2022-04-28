645d87093ac13060e002411fd759275aeefecb60
Alex <alexandrulapshin@gmail.com>
2022-04-28 17:20:08 -0400

    Add more btns and create basic structure

--- script.js
+++ script.js
@@ -7,22 +7,30 @@ function operate(operator,a,b){
     return operator(a,b);
 }
 
-const calcContainer = document.querySelector(".calculator-container");
+const calcMiddle = document.querySelector(".middle");
+const operatorsRight = document.querySelector(".operators-right");
 const digitsContainer = document.createElement("div");
-calcContainer.appendChild(digitsContainer);
+digitsContainer.classList.add("digits-container");
+calcMiddle.insertBefore(digitsContainer,operatorsRight);
 
 createDigitBtns();
-const dotBtn = document.createElement("button");
-dotBtn.textContent = ".";
-digitsContainer.appendChild(dotBtn);
-
 
 
 function createDigitBtns(){
-for(i=0; i<10; i++){
+for(i=1; i<=11; i++){
         const digit = document.createElement("button");
-        digit.classList.add("digit"+i);
-        digit.textContent = i;
+        if(i==10){
+            digit.classList.add("digit-0")
+            digit.textContent = "0";
+        }
+        else if(i==11){
+            digit.classList.add("digit-dot");
+            digit.textContent = ".";
+        }else{
+            digit.classList.add("digit-"+i);
+            digit.textContent = i;
+        }    
+
         digitsContainer.appendChild(digit);
         // digit.addEventListener("",);
     };
