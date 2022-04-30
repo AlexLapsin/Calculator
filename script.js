b7e3a036fac1a13fab68e8b0e08257553ca48771
Alex <alexandrulapshin@gmail.com>
2022-04-30 15:39:44 -0400

    Restructure DOM and add event listeners for btns

--- script.js
+++ script.js
@@ -7,31 +7,20 @@ function operate(operator,a,b){
     return operator(a,b);
 }
 
+let displayValue = 0;
+
 const calcMiddle = document.querySelector(".middle");
 const operatorsRight = document.querySelector(".operators-right");
-const digitsContainer = document.createElement("div");
-digitsContainer.classList.add("digits-container");
-calcMiddle.insertBefore(digitsContainer,operatorsRight);
-
-createDigitBtns();
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
 
 
-function createDigitBtns(){
-for(i=1; i<=11; i++){
-        const digit = document.createElement("button");
-        if(i==10){
-            digit.classList.add("digit-0")
-            digit.textContent = "0";
-        }
-        else if(i==11){
-            digit.classList.add("digit-dot");
-            digit.textContent = ".";
-        }else{
-            digit.classList.add("digit-"+i);
-            digit.textContent = i;
-        }    
+function displayBtn(btn){
+     display.textContent = btn.value;
+     displayValue = btn.value;
+}
 
-        digitsContainer.appendChild(digit);
-        // digit.addEventListener("",);
-    };
-};
+buttons.forEach(button => {
+    button.addEventListener("click", function(){displayBtn(this)});
+});
+
