8b5438e8a50a97cc8afd20d0fb124c15e40bc6b8
Alex <alexandrulapshin@gmail.com>
2022-04-30 20:19:57 -0400

    Add some functionality for add operation

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ function operate(operator,a,b){
     return operator(a,b);
 }
 
-let displayValue = 0;
+let displayValue = "";
+let tempArr = [];
+let firstVal = 0;
 
 const calcMiddle = document.querySelector(".middle");
 const operatorsRight = document.querySelector(".operators-right");
@@ -15,12 +17,39 @@ const display = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
 
 
-function displayBtn(btn){
-     display.textContent = btn.value;
-     displayValue = btn.value;
-}
+// function showVal(value){
+//     displayValue += value
+//     display.textContent = displayValue;
+// }
 
 buttons.forEach(button => {
-    button.addEventListener("click", function(){displayBtn(this)});
+    button.addEventListener("click", function(){
+
+        const val = this.value;
+        let currentVal = 0;
+        let sum = 0;
+
+        if(val == "+"){
+            //update the first value by doing the operation
+            currentVal = parseInt(tempArr.join(""));
+
+            sum = operate(add, firstVal, currentVal);
+            showVal(sum);
+            displayValue = sum;
+            firstVal = sum;
+
+            tempArr = [];
+            
+            
+        }
+        else{
+            tempArr.push(val);
+            showVal(val);
+        }
+    });
 });
 
+function clearDisplay(){
+    displayValue = "";
+    display.textContent = "";
+}
